e54be3ac3563de09ae40b34ab19608efa7396bac
anamsayyed016 <anamsayyed58@gmail.com>
2025-02-18 16:33:54 +0530

    advpyt

---
+++ hofuntion.py
@@ -0,0 +1,40 @@
+# map()---no of output is == output of element
+# filter()----based on condition----greater or equal
+# reduce()----single output
+
+# MAP()
+# def square (x):
+#     return x**2
+
+# l1=eval(input("Enter any numric value : "))
+# res=map(square,l1)
+# print(res)
+# print(list(res))
+
+# x=1,
+# print(type(x))
+
+# l1=(1,2,3,4,5,6,7,8,9,10)
+
+# def square(l1):
+#     return l1 ** 2
+
+# res=map(square,l1)
+# print(l1)
+
+# l1=[2,4,6]
+# l2=[1,2,3]
+# l3=[4,5,6,7]
+
+# def sum l1+l2+l3:
+
+# result = list(map(sum, 
+
+# l1=eval(input("Enter any numric value : "))
+# l1=eval(input("Enter any numric value : "))
+
+
+# square_l1=list(map(l1,[2]))
+# square_l2=list(map(l1,[2]))
+# print("Squared l1:", )
+# print("Squared l2:", )
