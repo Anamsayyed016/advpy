60689fcd7b959e4690ccd021399e2ad8fe2b7176
anamsayyed016 <anamsayyed58@gmail.com>
2025-02-19 14:27:22 +0530

    19febc++

--- hofuntion.py
+++ hofuntion.py
@@ -11,30 +11,21 @@
 # print(res)
 # print(list(res))
 
-# x=1,
-# print(type(x))
-
-# l1=(1,2,3,4,5,6,7,8,9,10)
-
-# def square(l1):
-#     return l1 ** 2
-
-# res=map(square,l1)
-# print(l1)
-
-# l1=[2,4,6]
-# l2=[1,2,3]
-# l3=[4,5,6,7]
-
-# def sum l1+l2+l3:
-
-# result = list(map(sum, 
+# def add(x,y,z):
+#     return x+y+z
 
 # l1=eval(input("Enter any numric value : "))
+# l2=eval(input("Enter any numric value : "))
+# l3=eval(input("Enter any numric value : "))
+
+# res=map(add,l1,l2,l3)
+# print(list(res))
+
+# def power (x,y):
+#     return x*y
 # l1=eval(input("Enter any numric value : "))
+# l2=eval(input("Enter any numric value : "))
+# res=map(power,l1,l2)
+# print(list(res))
 
 
-# square_l1=list(map(l1,[2]))
-# square_l2=list(map(l1,[2]))
-# print("Squared l1:", )
-# print("Squared l2:", )
---
+++ red.py
@@ -0,0 +1,14 @@
+import functools
+# syn:-function.reduce(fun_name, collection)
+
+# from functools import reduce
+# syn:- reduce(fun_name,collection)
+
+l1=eval(input("Enter any  value : "))
+def my_max(x,y):
+    if x<y:
+        return x
+    else:
+        return y
+res=functools.reduce(my_max,l1)
+print(res)
