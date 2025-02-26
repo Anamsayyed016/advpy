5d6de15fadcbd2daabbc81550bfd677bee0d9133
anamsayyed016 <anamsayyed58@gmail.com>
2025-02-26 14:26:19 +0530

    26febpy

---
+++ oops.py
@@ -0,0 +1,26 @@
+# class and object
+# connect of real world entities
+# obj is a real world entity
+# class blue print of obj
+# obj instance of class
+# onject physical existence of a class
+# basic syntax:- class Class_Name:
+                         # '''doc string'''
+                         # variables
+                         # method
+                # obj=Class_Name
+                # print(obj.method)
+                # print(obj.variable)
+
+
+# properties of oops:
+# abstarction
+# encapsulation
+                # data-protection-----class
+
+# inheritence
+# polymorphism
+                # code-reuseability----reuseability
+
+
+
