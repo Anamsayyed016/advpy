8c278e869334abf4673830f667656ef0798ba810
anamsayyed016 <anamsayyed58@gmail.com>
2025-02-28 14:29:24 +0530

    28febc++

---
+++ variableincls.py
@@ -0,0 +1,43 @@
+# # Intance Variable
+# # Static variable/class variable
+# # Local variable
+
+# # Instance Variable:-chnage variable and obj---first method is (self)
+
+# class Student:
+#     def __init__(self,name,roll,marks):
+#         self.x=name
+#         self.y=roll
+#         self.z=marks
+
+#     def add_new(self,city):
+#         self.p=city
+
+#     def show(self):
+#         print(self.x,self.y,self.z,self.p,self.school_name)
+
+# obj1=Student("Anam",101,70)
+# obj1.add_new('Bhopal')
+# obj1.school_name="SHSS"
+# obj1.show()
+# print(obj1.x,obj1.y,obj1.z,obj1.p,obj1.school_name)
+
+# ___________________________________________________________________________________
+
+class Members:
+    def __init__(self,name,qualification,number):
+        self .p = name
+        self .q = qualification
+        self .y = number
+    
+    def add_new(self,ids):
+        self.q = ids
+
+    def show(self):
+        print(self.p,self.q,self.y)
+    
+obj1=Members('ANam',"mba",100)
+obj1.add_new(100)
+obj1.show()
+print(obj1.p,obj1.q,obj1.y)
+        
