f2339a7a0d9e5b2348bee08552ea451f0f9fbbe6
anamsayyed016 <anamsayyed58@gmail.com>
2025-03-01 14:26:56 +0530

    1marpy

---
+++ statis_class_var.py
@@ -0,0 +1,29 @@
+# # did not change obj name or value
+
+# class Student:
+#     School="SHSS"
+#     # count=0
+
+#     def __init__(self,name,roll):
+
+#         self.x=name
+#         self.y=roll
+#         Student.s_code=123
+#         # count=count+1
+#         # Student.count=Student.count+1
+
+#     def new(self):
+#         Student.city="Bhopal"
+
+#     def show(self):
+#         print(Student.School,Student.s_code,Student.city,Student.subject)
+
+# Student.subject='PCM'
+
+# obj1=Student('Anam',101)
+# obj1.new()
+# obj1.show()
+# # print(Student.count)
+# obj1=Student('Anam',103)
+# # print(Student.count)
+# # print(School)
--- variableincls.py
+++ variableincls.py
@@ -1,43 +1,58 @@
-# # Intance Variable
-# # Static variable/class variable
-# # Local variable
+# # # Intance Variable
+# # # Static variable/class variable
+# # # Local variable
 
-# # Instance Variable:-chnage variable and obj---first method is (self)
+# # # Instance Variable:-chnage variable and obj---first method is (self)
 
-# class Student:
-#     def __init__(self,name,roll,marks):
-#         self.x=name
-#         self.y=roll
-#         self.z=marks
+# # class Student:
+# #     def __init__(self,name,roll,marks):
+# #         self.x=name
+# #         self.y=roll
+# #         self.z=marks
 
-#     def add_new(self,city):
-#         self.p=city
+# #     def add_new(self,city):
+# #         self.p=city
+
+# #     def show(self):
+# #         print(self.x,self.y,self.z,self.p,self.school_name)
+
+# # obj1=Student("Anam",101,70)
+# # obj1.add_new('Bhopal')
+# # obj1.school_name="SHSS"
+# # obj1.show()
+# # print(obj1.x,obj1.y,obj1.z,obj1.p,obj1.school_name)
+
+# # ___________________________________________________________________________________
+
+# class Members:
+#     def __init__(self,name,qualification,number):
+#         self .p = name
+#         self .q = qualification
+#         self .y = number
+    
+#     def add_new(self,ids):
+#         self.q = ids
 
 #     def show(self):
-#         print(self.x,self.y,self.z,self.p,self.school_name)
-
-# obj1=Student("Anam",101,70)
-# obj1.add_new('Bhopal')
-# obj1.school_name="SHSS"
-# obj1.show()
-# print(obj1.x,obj1.y,obj1.z,obj1.p,obj1.school_name)
-
-# ___________________________________________________________________________________
-
-class Members:
-    def __init__(self,name,qualification,number):
-        self .p = name
-        self .q = qualification
-        self .y = number
+#         print(self.p,self.q,self.y)
     
-    def add_new(self,ids):
-        self.q = ids
+# obj1=Members('ANam',"mba",100)
+# obj1.add_new(100)
+# obj1.show()
+# print(obj1.p,obj1.q,obj1.y)
+
+
+
+class Student:
+    def __init__(self,name,roll_no,marks):
+        
+        self.x = "Anam"
+        self.y = 102
+        self.z = 50
+
+    def add_new(self,city):
+        self.q
 
     def show(self):
-        print(self.p,self.q,self.y)
-    
-obj1=Members('ANam',"mba",100)
-obj1.add_new(100)
-obj1.show()
-print(obj1.p,obj1.q,obj1.y)
+        print(self.q,self.q,self.y)
         
