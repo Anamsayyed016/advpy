7c290c00de9f49cc9c68839f9580a66bb8af6bac
anamsayyed016 <anamsayyed58@gmail.com>
2025-03-05 14:27:12 +0530

    5febpy

--- Modulesfiles/opps_methodpro_inheri.py
+++ Modulesfiles/opps_methodpro_inheri.py
@@ -1,22 +1,34 @@
-# Inheritence------access parent class properties in child,code reuseabiliti,time saving, reduce-redendensy
+# # Inheritence------access parent class properties in child,code reuseabiliti,time saving, reduce-redendensy
 
-class A:
-    x=10
-    y=20
+# class A:
+#     x=10
+#     y=20
 
-    def home(self):
-        print("have a home")
+#     def home(self):
+#         print("have a home")
 
-    def car(self):
-        print("have a car")
+#     def car(self):
+#         print("have a car")
 
-class B(A):
+# class B(A):
 
-    def newhome(self):
-        print("New home")
+#     def newhome(self):
+#         print("New home")
 
-obj=B()
-obj.home()
-print(B.x)
-obj.car()
-obj.newhome()
+# obj=B()
+# obj.home()
+# print(B.x)
+# obj.car()
+# obj.newhome()
+
+# Types of inheritence:-
+
+# 1.Single Inheritance:-parent-----child
+
+# 2.Multiple Inheritance:-parent1 parent2-----------------child
+
+# 3.Multilevel Inheritance:-grand parent------parent------child1
+
+# 4.Herarichical Inheritance:-parent-----child1----child2
+
+# 5.Hybrid Inheritance:-
---
+++ Modulesfiles/tyfinherit.py
@@ -0,0 +1,80 @@
+# Types of inheritence:-
+
+# 1.Single Inheritance:-parent-----child
+
+# 2.Multiple Inheritance:-parent1 parent2-----------------child
+
+# 3.Multilevel Inheritance:-grand parent------parent------child1
+
+# 4.Herarichical Inheritance:-parent-----child1----child2
+
+# 5.Hybrid Inheritance:-
+# -----------------------------------------------------------------------------------------
+# Single Inheritance:-----
+
+# class Parent:
+#     x=10
+#     def home(self):
+#         print("Parent Home")
+
+# class Child(Parent):
+#     y=10
+#     def car(self):
+#         # print("Child car")
+#         super().home
+#         print("Child car")
+
+
+
+# obj=Child()
+# # print(obj.x)
+# # obj.home()
+# # obj.car()
+# obj.home()
+
+# ------------------------------------------------------------------------------------------------
+# method overwriting---write same method in child---supoort overwriting---using supermethod
+# does not support method over loading
+# -------------------------------------------------------------------------------------------------
+
+# Multiple Inheritance:-
+# MRO:-method resoultion order--left firt technic
+
+# class Parent1:
+#     def home(self):
+#         print("parent1 home")
+
+# class Parent2:
+#     def home(self):
+#         print("parent2 home")
+
+# class Child(Parent1,Parent2):
+#     def car(self):
+#         print("child car")
+
+
+# obj=Child()
+# obj.home()
+# -------------------------------------------------------------------------------------
+
+# Multilevel Inheritance:-
+
+
+class Grand_Parent:
+    def home(self):
+        print("parent1 home")
+
+class Parent(Grand_Parent):
+    def car(self):
+        print("parent2 home")
+
+class Child(Parent):
+    def new(self):
+        print("child new")
+
+
+obj=Child()
+obj.home()
+obj.new()
+obj.car()
+
