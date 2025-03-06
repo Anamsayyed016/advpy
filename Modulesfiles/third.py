5c198a2fe8b1fa0b0779931922d0fbe71da4c2a2
anamsayyed016 <anamsayyed58@gmail.com>
2025-03-06 14:27:34 +0530

    6febpy

--- Modulesfiles/third.py
+++ Modulesfiles/third.py
@@ -1,10 +1,10 @@
-# from first import x,y,z
-# from first import add
-from first import *
+# # from first import x,y,z
+# # from first import add
+# from first import *
 
 
-x=int(input("Enter the value:"))
-y=int(input("Enter the value:"))
-z=int(input("Enter the value:"))
-res=add(x,y,z)
-print(res)
+# x=int(input("Enter the value:"))
+# y=int(input("Enter the value:"))
+# z=int(input("Enter the value:"))
+# res=add(x,y,z)
+# print(res)
