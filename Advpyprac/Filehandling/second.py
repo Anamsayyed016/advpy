dd593164b357dd8311db1a994dcc6f29c2db8af3
anamsayyed016 <anamsayyed58@gmail.com>
2025-03-10 14:35:05 +0530

    10marcpp

---
+++ Advpyprac/Filehandling/second.py
@@ -0,0 +1,42 @@
+f=open('n1.txt1','rb')
+# print(f.name)
+# print(f.mode)
+
+# print(f.writable())
+# print(f.closed)
+
+# print(f.readable())
+# f.write("hello\n")
+
+# print(f.tell())
+# f.write("hello")
+
+# print(f.tell())
+
+data=f.read()
+print(data)
+print(f.tell())
+
+data=f.read()
+print(data)
+f.seek(-10,2)
+print(f.tell())
+data=f.read(8)
+print(data)
+
+# py reverse
+
+# obj in  mutable and unmutable
+# how many types oprestor in return---value---boolean
+# asignment opreator,====value return---
+# diff between while and for----infinite--while---finite---for
+# remote invre and devree it will go infinite
+# use break for infinite
+# string--default
+# funtion need---reuseibility
+# def and return----ternimate function
+# retrun is terminate function
+# *---tuple,**---dict
+# hof---
+# decorator
+# 
