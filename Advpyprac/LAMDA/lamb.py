82261d281f7b4731aae447d77e4702436829c075
anamsayyed016 <anamsayyed58@gmail.com>
2025-03-16 23:02:55 +0530

    16marpyprac

---
+++ Advpyprac/LAMDA/lamb.py
@@ -0,0 +1,22 @@
+# LAMDA:-
+# A function that doest have any name(ananymous function)
+# In this funtion define with lamda keyword instead of def
+# In this it takes number of argument and excute only one statement
+# In this excute only one line expression
+# ---------------------------------------------------
+# add = lambda x,y : print(x+y)
+# p=int(input("Enter any number:"))
+# q=int(input("Enter any number:"))
+# --------------------------------------------
+# square = lambda x,y :print(x*y)
+# p=int(input("Enter any number:"))
+# q=int(input("Enter any number:"))
+# square(p,q)
+# ---------------------------------------------------
+
+# n=int(input("Enter any number:"))
+# check_num = lambda x:'even_no' if x%2==0 else 'odd_no'
+# print(check_num(n))
+
+# --------------------------------------------------------
+
